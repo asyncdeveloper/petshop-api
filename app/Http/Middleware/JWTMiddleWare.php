fe0f9a81f4b66dfc020c268a54ab248cc86f56e5
asyncdeveloper <contact@asyncdeveloper.com>
2023-08-21 17:13:24 +0100

    setup tymon

---
+++ app/Http/Middleware/JWTMiddleWare.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use JWTAuth;
+use Symfony\Component\HttpFoundation\Response;
+
+class JWTMiddleWare
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        JWTAuth::parseToken()->authenticate();
+        return $next($request);
+    }
+}
