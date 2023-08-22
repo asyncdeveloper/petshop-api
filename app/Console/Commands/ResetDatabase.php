997d7089e61e2acea2cf615c11afb84710773845
asyncdeveloper <contact@asyncdeveloper.com>
2023-08-22 21:03:46 +0100

    Add ResetDatabase command

---
+++ app/Console/Commands/ResetDatabase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ResetDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reset-database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Truncate and re-seed database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Artisan::call('migrate:fresh --seed');
+    }
+}
