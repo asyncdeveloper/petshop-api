16d94d3c92c5fa7158114ae60d636302ade0f18b
asyncdeveloper <contact@asyncdeveloper.com>
2023-08-22 16:30:36 +0100

    add seed for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,14 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
@@ -18,11 +20,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'avatar' => fake()->name(),
+            'address' => fake()->address(),
+            'phone_number' => fake()->phoneNumber(),
+            'is_marketing' => false,
+            'is_admin' => false,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
         ];
     }
 
@@ -31,8 +38,22 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function isAdmin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'is_admin' => true,
+        ]);
+    }
+
+    public function isMarketing(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'is_marketing' => true,
         ]);
     }
 }
